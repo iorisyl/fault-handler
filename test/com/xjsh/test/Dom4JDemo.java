761d91e6a745011ee110105b39579b8a164fd6bd
iorisyl <214421782@qq.com>
2021-03-04 22:51:37 +0800

    初始版本

---
+++ test/com/xjsh/test/Dom4JDemo.java
@@ -0,0 +1,42 @@
+package com.xjsh.test;
+
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @Author: cxx
+ * Dom4j解析xml
+ * @Date: 2018/5/30 12:21
+ */
+public class Dom4JDemo {
+    public static void main(String[] args) throws Exception {
+        //1.创建Reader对象
+        SAXReader reader = new SAXReader();
+        //2.加载xml
+        Document document = reader.read(new File("D:\\录波文件\\芳华\\录波器\\220kV故障录波器B套DRL-600(芳华BH20)\\00020_00171_20210301_164519_295.HDR"));
+        //3.获取根节点
+        Element rootElement = document.getRootElement();
+        Iterator iterator = rootElement.elementIterator();
+        while (iterator.hasNext()){
+            Element stu = (Element) iterator.next();
+            List<Attribute> attributes = stu.attributes();
+            System.out.println("======获取属性值======");
+            for (Attribute attribute : attributes) {
+                System.out.println(attribute.getValue());
+            }
+            System.out.println("======遍历子节点======");
+            Iterator iterator1 = stu.elementIterator();
+            while (iterator1.hasNext()){
+                Element stuChild = (Element) iterator1.next();
+                System.out.println("节点名："+stuChild.getName()+"---节点值："+stuChild.getStringValue());
+            }
+        }
+    }
+}
